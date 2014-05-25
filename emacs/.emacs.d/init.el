ceee736e21effb0c4dfe71651e1ba0cf77498d2c
Chris Poole <chris@chrispoole.com>
2014-05-25 13:58:43 +0100

    Load private files before public ones

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -20,16 +20,16 @@
 (setq load-path (append (cjp-get-dir-structure-in "lisp")
                         load-path))
 
-;;; Load all my settings, as well as contributed functions
-(load-library "library-contributed")
-(load-library "cjp-settings")
-
 ;;; Load private files
 (mapc (lambda (file)
           (let ((absfile (concat "/Users/Chris/.emacs.d-private/"
                                  file)))
             (if (file-exists-p absfile) (load-file absfile))))
         '("cjp-library-private.el" "cjp-settings-private.el"))
+
+;;; Load all my settings, as well as contributed functions
+(load-library "library-contributed")
+(load-library "cjp-settings")
 
 ;;; Load custom faces
 (load-library "cjp-faces")
