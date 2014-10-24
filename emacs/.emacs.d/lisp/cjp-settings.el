c1519c57fbdc3714263d334d85b9cb0f6c51a415
Chris Poole <chris@chrispoole.com>
2014-10-24 09:43:31 +0100

    Update melpa URI

--- emacs/.emacs.d/lisp/cjp-settings.el
+++ emacs/.emacs.d/lisp/cjp-settings.el
@@ -70,7 +70,7 @@
 (require 'package)
 
 (setq package-user-dir (cjp-emacs-structure-dir "elpa"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (setq package-enable-at-startup nil)
 (setq package-load-list '(all))
