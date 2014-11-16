2a2c8b8a9163903597ff00e1922a63f51b9dbc1b
Chris Poole <chris@chrispoole.com>
2014-11-16 12:03:34 +0000

    Revert Emacs font on linux to Inconsolata: Menlo doesn't render well

--- emacs/.emacs.d/lisp/cjp-settings-linux.el
+++ emacs/.emacs.d/lisp/cjp-settings-linux.el
@@ -6,7 +6,7 @@
 
 ;;; Set font (Menlo default)
 (if (display-graphic-p)
-    (cjp-set-font-size "11" "Menlo" t))
+    (cjp-set-font-size "12" "Inconsolata" t))
 
 ;;; Move scrollbars to right side of frames
 ;(menu-bar-right-scroll-bar)
