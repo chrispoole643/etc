b457508c50cd0288cb7279fb21241b9fd5e8e344
Chris Poole <chris@chrispoole.com>
2015-02-28 16:21:52 +0000

    Enable helm auto-resize mode correctly

--- emacs/.emacs.d/lisp/cjp-settings.el
+++ emacs/.emacs.d/lisp/cjp-settings.el
@@ -143,6 +143,7 @@
 
 (require 'helm-config)
 (helm-mode 1)
+(helm-autoresize-mode t)
 
 (when (executable-find "curl")
   (setq helm-google-suggest-use-curl-p t))
@@ -151,8 +152,7 @@
       helm-move-to-line-cycle-in-source t
       helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
       helm-scroll-amount 8             ; scroll 8 lines other window using M-<next>/M-<prior>
-      helm-ff-file-name-history-use-recentf t
-      helm-autoresize-mode t)
+      helm-ff-file-name-history-use-recentf t)
 
 ;;; Fuzzy match where possible
 (setq helm-M-x-fuzzy-match t
